1f2a9af01fbaa90bea83dd2b6c1cd143af1e21e8
Juhil Aslaliya <76400010+juhil-aslaliya@users.noreply.github.com>
2023-01-30 14:09:16 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -105,6 +105,215 @@ class ApnaParser:
 
     def parse_BASE(self): 
         return self.parse_rassi()
+    
+    def parse_atom(self):
+        match self.lexer.peek_token():
+            case Identifier(name):  self.lexer.advance(); return Variable(name); 
+            case Num(value):        self.lexer.advance(); return NumLiteral(value); 
+            case Bool(value):       self.lexer.advance(); return BoolLiteral(value); 
+
+    def parse_exp(self):
+        left = self.parse_atom()
+        while True:
+            match self.lexer.peek_token():
+                case Operator("**"):
+                    self.lexer.advance()
+                    m = self.parse_atom()
+                    left = BinOp("**", left, m)
+                case _: break
+        return left
+
+    def parse_neg(self):
+        while True:
+            match self.lexer.peek_token():
+                case Operator("-"):
+                    self.lexer.advance()
+                    return -(self.parse_expr(self))
+                    # return -(self.parse_exp(self))
+                case _:
+                    # break
+                    return -(self.parse_exp(self))
+
+    def parse_mul(self):
+        left = self.parse_neg()
+        while True:
+            match self.lexer.peek_token():
+                case Operator(op) if op in (" * / // % ".split()):
+                    self.lexer.advance()
+                    m = self.parse_atom()
+                    left = BinOp(op, left, m)
+                case _: break
+        return left
+
+    def parse_add(self):
+        left = self.parse_mul()
+        while True:
+            match self.lexer.peek_token():
+                case Operator(op) if op in (" + - ".split()):
+                    self.lexer.advance()
+                    m = self.parse_mul()
+                    left = BinOp(op, left, m)
+                case _: break
+        return left
+
+    def parse_shift(self):
+        left = self.parse_add()
+        while True:
+            match self.lexer.peek_token():
+                case Operator(op) if op in (" >> << ".split()):
+                    self.lexer.advance()
+                    m = self.parse_atom()
+                    left = BinOp(op, left, m)
+                case _: break
+        return left
+
+    def parse_bnot(self):
+        while True:
+            match self.lexer.peek_token():
+                case Operator("~"):
+                    self.lexer.advance()
+                    return ~(self.parse_expr(self))
+                    # return ~(self.parse_not(self))
+                case _:
+                    # break
+                    return ~(self.parse_shift(self))
+
+    def parse_band(self):
+        left = self.parse_bnot()
+        while True:
+            match self.lexer.peek_token():
+                case Operator("&"):
+                    self.lexer.advance()
+                    m = self.parse_bnot()
+                    left = BinOp("&", left, m)
+                case _: break
+        return left
+
+    def parse_bxor(self):
+        left = self.parse_band()
+        while True:
+            match self.lexer.peek_token():
+                case Operator("^"):
+                    self.lexer.advance()
+                    m = self.parse_band()
+                    left = BinOp("^", left, m)
+                case _: break
+        return left
+
+    def parse_bor(self):
+        left = self.parse_bxor()
+        while True:
+            match self.lexer.peek_token():
+                case Operator("|"):
+                    self.lexer.advance()
+                    m = self.parse_bxor()
+                    left = BinOp("|", left, m)
+                case _: break
+        return left
+
+    def parse_cmp(self):
+        left = self.parse_bor()
+        match self.lexer.peek_token():
+            case Operator(op) if op in (" < > <= >= == != ".split()):
+                self.lexer.advance()
+                right = self.parse_bor()
+                return BinOp(op, left, right)
+        return left
+
+    def parse_lnot(self):
+        left = self.parse_cmp()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("not"):
+                    self.lexer.advance()
+                    m = self.parse_cmp()
+                    left = BinOp("not", left, m)
+                case _: break
+        return left
+
+    def parse_lnand(self):
+        left = self.parse_lnot()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("nand"):
+                    self.lexer.advance()
+                    m = self.parse_lnot()
+                    left = BinOp("nand", left, m)
+                case _: break
+        return left
+
+    def parse_land(self):
+        left = self.parse_lnand()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("and"):
+                    self.lexer.advance()
+                    m = self.parse_lnand()
+                    left = BinOp("and", left, m)
+                case _: break
+        return left
+
+    def parse_lxnor(self):
+        left = self.parse_land()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("xnor"):
+                    self.lexer.advance()
+                    m = self.parse_land()
+                    left = BinOp("xnor", left, m)
+                case _: break
+        return left
+
+    def parse_lxor(self):
+        left = self.parse_lxnor()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("xor"):
+                    self.lexer.advance()
+                    m = self.parse_lxnor()
+                    left = BinOp("xor", left, m)
+                case _: break
+        return left
+
+    def parse_lnor(self):
+        left = self.parse_lxor()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("nor"):
+                    self.lexer.advance()
+                    m = self.parse_lxor()
+                    left = BinOp("nor", left, m)
+                case _: break
+        return left
+
+    def parse_lor(self):
+        left = self.parse_lnor()
+        while True:
+            match self.lexer.peek_token():
+                case Keyword("or"):
+                    self.lexer.advance()
+                    m = self.parse_lnor()
+                    left = BinOp("or", left, m)
+                case _: break
+        return left
+
+    def parse_lassi(self):
+        left = self.parse_var()
+        match self.lexer.peek_token():
+            case Operator("<-"):
+                self.lexer.advance()
+                m = self.parse_lor()
+                left = BinOp("<-", left, m)
+        return left
+
+    def parse_rassi(self):
+        left = self.parse_lor()
+        match self.lexer.peek_token():
+            case Operator("->"):
+                self.lexer.advance()
+                m = self.parse_var()
+                right = BinOp("->", left, m)
+        return right
 
 
 
