d900d794ac8a7ea8f0dd14345c4b98b877bcc77c
dhruvpatel144 <76422222+dhruvpatel144@users.noreply.github.com>
2023-03-13 14:38:40 +0530

    Lexer
    The latest development of the lexer has resulted in notable advancements, specifically in the areas of string handling, operator recognition, boolean identification, keyword and identifier recognition, as well as support for integers, floats, and multi-operators (such as ">>" and ">="). With these updates, a new list of tokens is generated for each line of code within the language, which is then compiled into a nested list for comprehensive parsing. As a result, the lexer is now capable of fully processing code files and producing a complete and accurate nested list of tokens for further analysis and manipulation. These improvements represent significant strides towards enhancing the functionality and versatility of the lexer, and have established a more robust foundation for continued development and refinement in the future.

--- lexer.py
+++ lexer.py
@@ -1,7 +1,7 @@
 from fractions import Fraction
 import re
 from dataclasses import dataclass
-from typing import Optional, NewType
+from typing import List
 
 # A minimal example to illustrate typechecking.
 
@@ -60,13 +60,17 @@ class Operator:
 class Float:
     f: float
 
+@dataclass
+class end_of_all_tokens(Exception):
+    e: str
+
 Token = Int | Bool | Keyword | Identifier | Operator | Float
 
-class EndOfTokens(Exception):
+class EndOfTokens(Exception): 
     pass
 
-keywords = "Int String Float Bool let if then else elif endif fun of is endfun print".split()
-symbolic_operators = "+ - * / < > <= >= == != ** % // ~ & | ^ -> <- << >>".split()
+keywords = "Int String Float Bool let in if then else elif endif fun of is endfun print for endfor while returns do endwhile".split()
+symbolic_operators = "+ - * / < > <= >= = ==  ! != ** % // ~ & | ^ -> <- << >> ( )".split()
 word_operators = "and or not xor xnor nand nor concat from to".split()
 whitespace = " \t \n".split() + [' ']
 
@@ -75,24 +79,26 @@ def word_to_token(word):
         return Keyword(word)
     if word in word_operators:
         return Operator(word)
+    if word in symbolic_operators:
+        return Operator(word)
     if word == "True":
         return Bool(True)
     if word == "False":
         return Bool(False)
     a = None
     try:
-        a = int(word)
-        return Int(a)
-    except ValueError:
-        pass
-    try:
         a = float(word)
         return Float(a)
     except ValueError:
         pass
-    ptrn = "^[_a-zA-Z][a-zA-Z0-9_]*$"
+    ptrn = "^[_a-zA-Z][_a-zA-Z0-9]*$"
     if(re.match(ptrn, word)):
         return Identifier(word)
+    try:
+        a = int(word)
+        return Int(a)
+    except ValueError:
+        pass
     return UnknownWord(word)
 
 
@@ -120,38 +126,67 @@ class Lexer:
                                 s = s + c
                             else:
                                 self.stream.unget()
+                                # print(Operator(s))
                                 return Operator(s)
                         except EndOfStream:
+                            # print(Operator(s))
                             return Operator(s)
-
-                case c if c.isdigit():
-                    n = int(c)
-                    while True:
-                        try:
-                            c = self.stream.next_char()
-                            if c.isdigit():
-                                n = n*10 + int(c)
-                            else:
-                                self.stream.unget()
-                                return Int(n)
-                        except EndOfStream:
-                            return Int(n)
                 case c if c.isalpha() or c=='_':
                     s = c
                     while True:
                         try:
                             c = self.stream.next_char()
-                            if c.isalpha() or c=='_':
+                            if c.isalpha() or c=='_' or c.isdigit():
                                 s = s + c
                             else:
                                 self.stream.unget()
+                                # print(word_to_token(s))
                                 return word_to_token(s)
                         except EndOfStream:
+                            # print(word_to_token(s))
                             return word_to_token(s)
+                case c if c.isdigit():
+                    n = int(c)
+                    decimal_point = False
+                    while True:
+                        try:
+                            c = self.stream.next_char()
+                            # print(c)
+                            if ((c.isdigit()) and (decimal_point == False)):
+                                n = n*10 + int(c)
+                            # currently not handling the error of double decimal point.
+                            elif ((c.isdigit()) and (decimal_point == True)):
+                                n = n+int(c)*factor
+                                factor = 0.1*factor
+                            elif((c == '.') and (decimal_point == False)):
+                                decimal_point = True
+                                factor = 0.1
+                            else:
+                                self.stream.unget()
+                                if (decimal_point):                                    
+                                    return Float(n)
+                                else:
+                                    return Int(n)
+                        except EndOfStream:
+                            if(decimal_point):
+                                return Float(n)
+                            return Int(n)
+                case c if c =='"':
+                    s = c
+                    c = self.stream.next_char()
+                    while(c!= '"'):
+                        s = s+c
+                        c = self.stream.next_char()
+                    s = s+c
+                    return String(s)
                 case c if c in whitespace:
                     return self.next_token()
+                case _:
+                    pass
+        
+                
         except EndOfStream:
-            raise EndOfTokens
+           raise EndOfTokens
 
     def peek_token(self) -> Token:
         if self.save is not None:
@@ -176,3 +211,35 @@ class Lexer:
             return self.next_token()
         except EndOfTokens:
             raise StopIteration
+def print_tokens(code: str):
+    strs = code.split(';')
+    l= []
+    for i in range(len(strs)):
+        code = strs[i]
+        stream = Stream.from_string(code)
+        lexer = Lexer(stream)
+        tokens = []
+        try:
+            while True:
+                token = lexer.next_token()
+                if(token==None):
+                    continue
+                tokens.append(token)
+                # print(token)
+        except EndOfTokens:
+            pass
+        l.append(tokens)
+    l[i].append(end_of_all_tokens("end_of_tokens"))
+    return l
+
+file_path = r"C:\Users\dhruv\Downloads\test.txt"
+
+with open(file_path, 'r') as file:
+    file_contents = file.read()
+
+# print(file_contents)
+
+
+
+ans = print_tokens(file_contents)
+print(ans)
