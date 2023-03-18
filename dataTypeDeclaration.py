25f62388ba2d7154a2a8fa62df43c34de500a89b
Meet <76533212+meetv25@users.noreply.github.com>
2023-03-18 13:07:03 +0530

    Updated Keywords & other lists towards EOF

--- dataTypeDeclaration.py
+++ dataTypeDeclaration.py
@@ -205,11 +205,13 @@ Value = Fraction | bool | int | float | str | None
 ###############################################################################################################################################
 
 
-keywords = "   if then elif else endif    let    Int Float String Bool Frac   ".split()
+keywords = " Int Float Frac Bool String    let var   if then elif else endif    print    fun of is returns endfun   while do endwhile   for in endfor   concat ".split()
 
 symbolic_operators = "  + - * / // % **    < <= => > == !=    >> <<    ~ & | ^     <- ->  ".split()
 
 word_operators = " and or not xor xnor nand nor ".split()           # Logical Operators
+
+brackets = " ( ) [ ] ".split()
 
 whitespace = "\t \n".split() + [' ']
 
